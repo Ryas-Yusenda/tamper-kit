6ebda097cf1b9c578794cd830d7e39cb98ba1ff8
Ryas-Yusenda <44300598+Ryas-Yusenda@users.noreply.github.com>
2025-09-13 11:14:40 +0700

    feat: Wallhaven Enhance

---
+++ wallhaven-enhance.user.js
@@ -0,0 +1,188 @@
+// ==UserScript==
+// @name         Wallhaven Enhance
+// @namespace    https://github.com/Ryas-Yusenda/tamper-kit
+// @version      1.0.0
+// @description  Wallhaven Enhancements: Download button on thumbnails, LightGallery integration, and more.
+// @author       Ry-ys
+// @include      https://wallhaven.cc/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=wallhaven.cc
+// @grant        none
+// @run-at       document-end
+// @license      MIT
+// @updateURL    https://github.com/Ryas-Yusenda/tamper-kit/raw/main/wallhaven-enhance.user.js
+// @downloadURL  https://github.com/Ryas-Yusenda/tamper-kit/raw/main/wallhaven-enhance.user.js
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // inject CSS & JS
+  const injectCSS = url => {
+    const l = document.createElement('link');
+    l.rel = 'stylesheet';
+    l.href = url;
+    document.head.appendChild(l);
+  };
+  const injectJS = url =>
+    new Promise(r => {
+      const s = document.createElement('script');
+      s.src = url;
+      s.onload = () => r();
+      document.head.appendChild(s);
+    });
+
+  // load LightGallery
+  async function loadLightGallery() {
+    injectCSS('https://cdn.staticfile.org/lightgallery/1.6.12/css/lightgallery.min.css');
+    await injectJS('https://cdn.staticfile.org/lightgallery/1.6.12/js/lightgallery-all.min.js');
+    console.log('LightGallery loaded!');
+  }
+
+  // download util
+  const forceDownload = (url, fileName) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'blob';
+    xhr.onload = function () {
+      const u = (window.URL || window.webkitURL).createObjectURL(this.response),
+        a = document.createElement('a');
+      a.href = u;
+      a.download = fileName;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    };
+    xhr.send();
+  };
+
+  class Pic {
+    constructor(elem, ws) {
+      this.elem = elem;
+      this.ws = ws;
+      const $p = $(elem);
+      this.favs = parseInt($p.find('.wall-favs')[0].innerHTML);
+      this.seen = $p.find('figure').hasClass('thumb-seen');
+      this.id = $p.find('figure').data('wallpaper-id');
+      this.isPNG = $p.find('span.png').length > 0;
+      const f = this.id.substring(0, 2);
+      this.picUrl = `https://w.wallhaven.cc/full/${f}/wallhaven-${this.id}.jpg`;
+      this.name = this.id + '.jpg';
+      if (this.isPNG) {
+        this.picUrl = this.picUrl.replace('.jpg', '.png');
+        this.name = this.name.replace('.jpg', '.png');
+      }
+    }
+    addDownload() {
+      const dl = $(`<a class="jsDownload" href="javascript:;"><i class="fa fa-fw fa-cloud-download"></i></a>`)[0];
+      dl.onclick = this.download.bind(this);
+      $(this.elem).find('.thumb-info').append(dl);
+    }
+    download() {
+      forceDownload(this.picUrl, this.name);
+    }
+    initGallery() {
+      const $p = $(this.elem).find('figure');
+      $p.data('data-src', this.picUrl).data('data-sub-html-url', 'https://wallhaven.cc/w/' + this.id);
+      $p.click(this.showGallery);
+    }
+    showGallery() {
+      return false;
+    }
+  }
+
+  class WallhavenScript {
+    constructor() {
+      this.download = this.gallery = this.maxView = true;
+      this.isLogined = $('#userpanel > a > span.username').length > 0;
+    }
+    workList() {
+      this.workListMain();
+      new MutationObserver(this.workListMain.bind(this)).observe(document.body, { childList: true, subtree: true });
+    }
+    workListMain() {
+      const pics = this.getPics(),
+        newPics = this.filterNewPics(pics);
+      for (const p of newPics) {
+        if (this.download) p.addDownload();
+        if (this.gallery) p.initGallery();
+      }
+      this.pics = pics;
+    }
+    workSingle() {
+      if (this.maxView) {
+        $('#header, #searchbar').hide('fast');
+        $('#showcase-sidebar').animate({ top: 0 }, 'fast');
+        $('#main').animate({ borderTopWidth: 0 }, 'fast');
+        $('#wallpaper').animate({ maxWidth: '99%', maxHight: '99%' }, 'fast');
+      }
+    }
+    getPics() {
+      return $('.thumb-listing-page li')
+        .toArray()
+        .map(e => new Pic(e, this));
+    }
+    filterNewPics(pics) {
+      const old = this.pics.map(p => p.elem);
+      return pics.filter(p => old.indexOf(p.elem) < 0);
+    }
+    run() {
+      if (location.pathname.indexOf('/w/') === 0) return this.workSingle();
+      this.pics = [];
+      return this.workList();
+    }
+  }
+
+  // Run the main
+  new WallhavenScript().run();
+
+  // Fancybox loader
+  const loadScript = () =>
+      new Promise(r => {
+        const s = document.createElement('script');
+        s.src = 'https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js';
+        document.head.appendChild(s);
+        s.onload = () => r();
+      }),
+    loadStylesheet = () =>
+      new Promise(r => {
+        const l = document.createElement('link');
+        l.rel = 'stylesheet';
+        l.href = 'https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css';
+        document.head.appendChild(l);
+        l.onload = () => r();
+      }),
+    loadFancybox = async () => {
+      await loadScript();
+      await loadStylesheet();
+    };
+
+  const callFancyBox = () => {
+    loadFancybox().then(() => {
+      for (const el of document.querySelectorAll('.thumbs-container ul li')) {
+        const preview = el.querySelector('.preview'),
+          ext = el.querySelector('.thumb-info span.png') ? 'png' : 'jpg';
+        if (preview.getAttribute('data-href')) continue;
+        const id = /wallhaven\.cc\/w\/(\w{6})/.exec(preview.href)[1],
+          path = id.substring(0, 2);
+        preview.setAttribute('data-href', preview.href);
+        preview.setAttribute('data-fancybox', 'gallery');
+        preview.href = `https://w.wallhaven.cc/full/${path}/wallhaven-${id}.${ext}`;
+      }
+      $('body').on('click', '[data-fancybox-download]', e => {
+        const url = $(e.target).parent()[0].href,
+          name = url.substr(url.lastIndexOf('/') + 1);
+        forceDownload(url, name.replace('wallhaven-', ''));
+        e.preventDefault();
+      });
+      $("[data-fancybox='gallery']").fancybox({ buttons: ['zoom', 'share', 'slideShow', 'fullScreen', 'download', 'thumbs', 'close'], thumbs: { autoStart: true } });
+    });
+  };
+
+  callFancyBox();
+  new MutationObserver(m => {
+    if (m[0].addedNodes.length && m[0].addedNodes[0].className === 'thumb-listing-page') callFancyBox();
+  }).observe(document.querySelector('.thumbs-container'), { childList: true, characterDataOldValue: true });
+
+  // Load Lightgallery at the beginning
+  loadLightGallery();
+})();
