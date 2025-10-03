f7ab897afe38377ab610a63d047eb8e18266e595
Ryas-Yusenda <44300598+Ryas-Yusenda@users.noreply.github.com>
2025-10-03 12:27:11 +0700

    feat: Update ChatGPT Soft Green Theme to version 2.0.0

--- chat-gpt-theme.user.js
+++ chat-gpt-theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT Soft Green Theme
 // @namespace    https://github.com/Ryas-Yusenda/tamper-kit
-// @version      1.1.1
+// @version      2.0.0
 // @description  Change ChatGPT custom theme to soft green
 // @author       Ry-ys
 // @match        *://*.chatgpt.com/*
@@ -18,18 +18,17 @@
 
   const style = document.createElement('style');
   style.innerHTML = `
-        [data-chat-theme=green], [data-chat-theme=green] .dark {
-            --theme-attribution-highlight-bg: #ffffff33 !important;
-            --theme-entity-accent: #f9f9f9 !important;
-            --theme-mini-msg-bg: #4cafa380 !important;
-            --theme-secondary-btn-bg: #f0f0f0 !important;
-            --theme-secondary-btn-text: #4cafa3 !important;
-            --theme-submit-btn-bg: #f0f0f0 !important;
-            --theme-submit-btn-text: #4cafa3 !important;
-            --theme-user-msg-bg: #4cafa380 !important;
-            --theme-user-msg-text: #f9f9f9 !important;
-            --theme-user-selection-bg: #ffffff33 !important;
-        }
-    `;
+    [data-chat-theme=green], [data-chat-theme=green] .dark {
+      --theme-attribution-highlight-bg:#ffffff33!important;
+      --theme-entity-accent:#f9f9f9!important;
+      --theme-mini-msg-bg:#4cafa380!important;
+      --theme-secondary-btn-bg:#f0f0f0!important;
+      --theme-secondary-btn-text:#4cafa3!important;
+      --theme-submit-btn-bg:#f0f0f0!important;
+      --theme-submit-btn-text:#4cafa3!important;
+      --theme-user-msg-bg:#4cafa380!important;
+      --theme-user-msg-text:#f9f9f9!important;
+      --theme-user-selection-bg:#ffffff33!important;
+    }`;
   document.head.appendChild(style);
 })();
